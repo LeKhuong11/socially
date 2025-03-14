00b59bbc80ce1d9525e6113955327ef7bcec6e01
Khuong <113273030+LeKhuong11@users.noreply.github.com>
2025-03-14 22:50:09 +0700

    fix: not use

--- src/app/[locale]/login/page.tsx
+++ src/app/[locale]/login/page.tsx
@@ -6,10 +6,14 @@ import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { LoaderCircle } from 'lucide-react'
 import Link from 'next/link'
-import React from 'react'
+import React, { useEffect } from 'react'
 
 function Login() {
     const [processing, setProcessing] = React.useState(false);
+
+    useEffect(() => {
+        setProcessing(false);
+    }, []);
   return (
     <div>
       <form className="flex flex-col gap-6">
